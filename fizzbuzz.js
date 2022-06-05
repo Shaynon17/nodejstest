3dab727b291904992584ffa821e52a6ef53eab92
shaynon17 <shannon.wlmiller@yahoo.com>
2022-06-05 08:51:44 -0500

    fizzbuzz practice

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,8 +1,18 @@
-
+// have 3 = fizz
+//have 5 = buzz
+//have 15 = fizzbuzz
 module.exports.fizzbuzzConverter = (number) => {
-    if (number === 3) {
+    if (isDivisbleBy(number, 5) && isDivisbleBy(number, 3)) {
+        return 'fizzbuzz'
+    } else if (isDivisbleBy(number, 3)) {
         return "fizz"
+    } else if (isDivisbleBy(number, 5)) {
+        return "buzz"
     } else {
         return number;
     }
+}
+
+function isDivisbleBy(larger, small) {
+    return (larger % small === 0)
 }
--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -7,3 +7,23 @@ test("fizzbuzzConverter(1) will return 1", () => {
 test("fizzbuzzConverter(3) will return 'fizz'", () => {
     expect(fizzbuzzConverter(3)).toBe('fizz');
 })
+
+test('fizzbuzzconverter(5) will return "buzz"', () => {
+    expect(fizzbuzzConverter(5)).toBe('buzz');
+})
+
+test('fizzbuzzconverter(15) will return "buzz"', () => {
+    expect(fizzbuzzConverter(15)).toBe('fizzbuzz');
+})
+
+test('fizzbuzzconverter(10) will return "buzz"', () => {
+    expect(fizzbuzzConverter(10)).toBe('buzz');
+})
+
+test('fizzbuzzconverter(9) will return "buzz"', () => {
+    expect(fizzbuzzConverter(9)).toBe('fizz');
+})
+
+test('fizzbuzzconverter(150) will return "buzz"', () => {
+    expect(fizzbuzzConverter(150)).toBe('fizzbuzz');
+})
