088911712f1c2a34fc8a7a5f975ae9ff39d951aa
Shannon Miller <shannon.wlmiller@yahoo.com>
2022-11-13 18:48:59 -0600

    basic functions added for conflict

--- gameWebsite/ohShoot/index.js
+++ gameWebsite/ohShoot/index.js
@@ -1,3 +1,18 @@
+// must be in this directiory PS C:\Users\shann\coding\nodejstest\gameWebsite\ohShoot> then to rune code type in terminal: node index.js
+
+function getUserChoice(userInput) {
+    userInput = userInput.toLowercase();
+    if (userInput === 'shoot' ||
+        userInput === 'reload' ||
+        userInput === 'block' ||
+        userInput === 'punch' 
+        ) {
+        return userInput;
+    } else {
+        console.log('error');
+    }
+}
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 4);
     if (randomNumber === 0) {
@@ -11,4 +26,64 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
+// // function getUserChoice
+
+// function player1() {
+//     let bullets = {
+//         holster: 5,
+//         clip: 0,
+//     }
+//     return bullets
+//     //the below is a better output 
+//     // return `player 1 has ${bullets.holster} bullets in their holster, and
+//     // ${bullets.clip} bullets in their clip`
+// }
+
+// function player2() {
+//     let bullets = {
+//         holster: 5,
+//         clip: 0,
+//     }
+//     return bullets
+// }
+
+// function bulletTransfer() {
+//     player3.holster -= 1
+//     return player3
+// }
+
+function bulletTransfer(loser, winner) {
+loser.holster -= 1;
+winner.holster += 1;
+}
+
+function reload(reloader) {
+    reloader.holster -= 1;
+    reloader.clip += 1;
+}
+
+let player1 = {
+    holster: 6,
+    clip: 0
+}
+
+let player2 = {
+    holster: 5,
+    clip: 0
+}
+
+function shoot(shooter) {
+    shooter.clip -= 1;
+    shooter.holster += 1;
+}
+
+// bulletTransfer(player2, player1)
+// reload(player1)
+// shoot(player1)
+// reload(player1)
+
+// console.log(getComputerChoice())
+// console.log(player1())
+console.log(player1)
+console.log(player2)
+// console.log(bulletTransfer())
