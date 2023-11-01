27199684717ba9b0c340531330b6032132f80824
Shannon Miller <shannon.wlmiller@yahoo.com>
2023-10-31 19:27:25 -0600

    working on nested loops

--- poker.js
+++ poker.js
@@ -36,8 +36,42 @@ const parseHand = hand => { //hand = "3S AH"
     return output
 }
 
+// const handFaceValues = hand => {
+//     let parsedHand = parseHand(hand); // [{ suit: 'S', face: 6 }, { suit: 'H', face: 14 },{ suit: 'C', face: 3 }, { suit: 'C', face: 5 },{ suit: 'S', face: 3 } ]
+//     let allValues = [] // allValues = [ 6, 14, 3, 5, 3 ] (top hand)
+//     for (i = 0; i < parsedHand.length; i++) {
+//         let faceValue = parsedHand[i].face;
+//         allValues.push(faceValue)
+//     }
+//     return allValues
+// }
+
+
+//loop through each card and compare cards with a nested loop
+const detectPair = hand => {
+    let parsedHand = parseHand(hand); // [{ suit: 'S', face: 6 }, { suit: 'H', face: 14 },{ suit: 'C', face: 3 }, { suit: 'C', face: 5 },{ suit: 'S', face: 3 } ]
+    let allValues = []     // allValues = [ 6, 14, 3, 5, 3 ] (top hand)
+    for (i = 0; i < parsedHand.length; i++) {
+        let faceValue = parsedHand[i].face;
+        allValues.push(faceValue)
+    }
+let array = allValues
+    for (i = 0; i < array.length; i++) {
+        for (j = 0; j < array.length; i++) {
+            if (i !== j) {
+                if (array[i] === array[j]) {
+                    return true
+                } else {
+                    continue;
+                }
+            }
+        }
+    }
+
+}
+
 
 
 module.exports = {
-    parseHand, parseCard, parseFaceValue, parseSuit,
+    detectPair, parseHand, parseCard, parseFaceValue, parseSuit,
 }
