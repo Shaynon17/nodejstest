321990de4217fd91e9207c7ef56028ccb3e0e34e
Shannon Miller <shannon.wlmiller@yahoo.com>
2023-11-02 13:40:47 -0600

    had to work on wireframe before continuing

--- gameWebsite/ohShoot/index.js
+++ gameWebsite/ohShoot/index.js
@@ -1,34 +1,63 @@
 // must be in this directiory PS C:\Users\shann\coding\nodejstest\gameWebsite\ohShoot> then to rune code type in terminal: node index.js
 
-function getUserChoice(userInput) {
-    userInput = userInput.toLowercase();
-    if (userInput === 'shoot' ||
-        userInput === 'reload' ||
-        userInput === 'block' ||
-        userInput === 'punch' 
-        ) {
-        return userInput;
-    } else {
-        console.log('error');
-    }
-}
 
-function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 4);
-    if (randomNumber === 0) {
-        return 'punch'
-    } else if (randomNumber === 1) {
-        return 'reload'
-    } else if (randomNumber === 2) {
-        return 'block'
-    } else if (randomNumber === 3) {
-        return 'shoot'
-    }
-    //for getComputerChoice, if the clip has 0 then let randomNumber = Math.floor(Math.random() * 4)
+// let playerLoaded = '';
+let playerHolster = 5;
+
+// let enemyLoaded = '';
+// let enemyHolster = 5;
+
+
+const reload = () => {
+    playerHolster -= 1;
+    document.getElementById("playerHolster").innerHTML = playerHolster;
 }
 
 
-// // function getUserChoice
+document.getElementById("reloadBtn").onclick = function () {
+    // count -= 10;
+    playerHolster -= 1;
+    document.getElementById("playerHolster").innerHTML = playerHolster;
+}
+
+document.getElementById("ninetyNine_decreaseBtn").onclick = function () {
+    count -= 10;
+    document.getElementById("ninetyNine_countLabel").innerHTML = count;
+}
+
+
+
+
+//old docuement for reference IF i need it
+// function getUserChoice(userInput) {
+//     userInput = userInput.toLowercase();
+//     if (userInput === 'shoot' ||
+//         userInput === 'reload' ||
+//         userInput === 'block' ||
+//         userInput === 'punch'
+//         ) {
+//         return userInput;
+//     } else {
+//         console.log('error');
+//     }
+// }
+
+// function getComputerChoice() {
+//     let randomNumber = Math.floor(Math.random() * 4);
+//     if (randomNumber === 0) {
+//         return 'punch'
+//     } else if (randomNumber === 1) {
+//         return 'reload'
+//     } else if (randomNumber === 2) {
+//         return 'block'
+//     } else if (randomNumber === 3) {
+//         return 'shoot'
+//     }
+//     //for getComputerChoice, if the clip has 0 then let randomNumber = Math.floor(Math.random() * 4)
+// }
+
+
+// // // function getUserChoice
 
 // function player1() {
 //     let bullets = {
@@ -36,7 +65,7 @@ function getComputerChoice() {
 //         clip: 0,
 //     }
 //     return bullets
-//     //the below is a better output 
+//     //the below is a better output
 //     // return `player 1 has ${bullets.holster} bullets in their holster, and
 //     // ${bullets.clip} bullets in their clip`
 // }
@@ -54,30 +83,30 @@ function getComputerChoice() {
 //     return player3
 // }
 
-function bulletTransfer(loser, winner) {
-loser.holster -= 1;
-winner.holster += 1;
-}
+// function bulletTransfer(loser, winner) {
+// loser.holster -= 1;
+// winner.holster += 1;
+// }
 
-function reload(reloader) {
-    reloader.holster -= 1;
-    reloader.clip += 1;
-}
+// function reload(reloader) {
+//     reloader.holster -= 1;
+//     reloader.clip += 1;
+// }
 
-let player1 = {
-    holster: 6,
-    clip: 0
-}
+// let player1 = {
+//     holster: 6,
+//     clip: 0
+// }
 
-let player2 = {
-    holster: 5,
-    clip: 0
-}
+// let player2 = {
+//     holster: 5,
+//     clip: 0
+// }
 
-function shoot(shooter) {
-    shooter.clip -= 1;
-    shooter.holster += 1;
-}
+// function shoot(shooter) {
+//     shooter.clip -= 1;
+//     shooter.holster += 1;
+// }
 
 
 
