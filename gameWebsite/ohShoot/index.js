b3f7dc014347603981fe545072ebffaae38a2783
Shannon Miller <shannon.wlmiller@yahoo.com>
2023-11-02 16:50:56 -0600

    wrote the 3 algorithms for enemy choice

--- gameWebsite/ohShoot/index.js
+++ gameWebsite/ohShoot/index.js
@@ -1,6 +1,5 @@
 // must be in this directiory PS C:\Users\shann\coding\nodejstest\gameWebsite\ohShoot> then to rune code type in terminal: node index.js
 
-
 // let playerLoaded = 0;
 // let playerHolster = 5;
 // let enemyLoaded = 0;
@@ -18,24 +17,136 @@ const enemy = {
     holster: 5,
 }
 
+//alogorithm 1 : reload: 40% | punch: 40% | block: 20% 
+const preparative = () => {
+    let choice = Math.floor(Math.random() * 5);
+    let decision = '?'
+    if (choice < 2) {
+        decision = "reload"
+        document.getElementById("enemyAction").innerHTML = "reloaded";
+        // enemy.loaded = 1;
+        // enemy.holster -= 1;
+        // document.getElementById("enemyLoaded").innerHTML = player.loaded
+        // document.getElementById("enemyHolster").innerHTML = player.holster;
+    } else if (choice >= 2 && choice < 4) {
+        decision = 'punch'
+        document.getElementById("enemyAction").innerHTML = "punched"
+    } else {
+        decision = 'block'
+        document.getElementById("enemyAction").innerHTML = "blocked"
+    }
+    return decision
+}
 
-// function checkIfLoaded() {
-//     if
+//alogorithm 2 : shoot: 33% | punch: 33% | block: 33% 
+const agressive = () => {
+    let choice = Math.floor(Math.random() * 3);
+    let decision = '?'
+    if (choice === 0) {
+        decision = "shoot"
+        document.getElementById("enemyAction").innerHTML = "shoot";
+        // enemy.loaded -= 1;
+        // document.getElementById("enemyLoaded").innerHTML = player.loaded
+        // document.getElementById("enemyHolster").innerHTML = player.holster;
+    } else if (choice === 1) {
+        decision = 'punch'
+        document.getElementById("enemyAction").innerHTML = "punched"
+    } else {
+        decision = 'block'
+        document.getElementById("enemyAction").innerHTML = "blocked"
+    }
+    return decision
+}
+
+//alogorithm 3 : reload: 33% | punch: 33% | block: 33% 
+const defensive = () => {
+    let choice = Math.floor(Math.random() * 3);
+    let decision = '?'
+    if (choice === 0) {
+        decision = "reload"
+        document.getElementById("enemyAction").innerHTML = "reload";
+        // enemy.loaded = 1;
+        // enemy.holster -= 1;
+        // document.getElementById("enemyLoaded").innerHTML = player.loaded
+        // document.getElementById("enemyHolster").innerHTML = player.holster;
+    } else if (choice === 1) {
+        decision = 'punch'
+        document.getElementById("enemyAction").innerHTML = "punched"
+    } else {
+        decision = 'block'
+        document.getElementById("enemyAction").innerHTML = "blocked"
+    }
+    return decision
+    // return decision && document.getElementById("enemyAction").innerHTML = `${decision}`
+}
+
+
+const enemyChoice = () => {
+
+    if (player.loaded === 0 && enemy.loaded === 0) {
+        return preparative()
+    }
+
+}
+
+
+function resetGame() {
+    player.loaded = 0,
+        player.holster = 5,
+        enemy.loaded = 0,
+        enemy.holster = 5,
+        document.getElementById("liveAction").innerHTML = "Duel!";
+    document.getElementById("playerLoaded").innerHTML = 0;
+    document.getElementById("playerHolster").innerHTML = 5;
+    document.getElementById("enemyLoaded").innerHTML = 0;
+    document.getElementById("enemyHolster").innerHTML = 5;
+    document.getElementById("enemyAction").innerHTML = '';
+
+}
+
+// const enemyChoice = () => {
+//     let factor = 3
+//     let choice = Math.floor(Math.random() * factor);
+
 // }
 
+const bothNotLoaded = () => {
+
+}
+
+
+const enemyIsNotLoaded = () => {
+    let choice = Math.floor(Math.random() * 4);
+    let decision = '?'
+    if (choice < 2) {
+        decision = "reload"
+        document.getElementById("enemyAction").innerHTML = "reloaded";
+        enemy.loaded = 1;
+        enemy.holster -= 1;
+        document.getElementById("enemyLoaded").innerHTML = player.loaded
+        document.getElementById("enemyHolster").innerHTML = player.holster;
+    } else if (choice >= 2 && choice < 4) {
+        decision = 'punch'
+        document.getElementById("enemyAction").innerHTML = "punched"
+    } else {
+        decision = 'block'
+        document.getElementById("enemyAction").innerHTML = "blocked"
+    }
+    return decision
+}
 
 const reload = () => {
-
     if (player.loaded === 1) {
-        document.getElementById("liveAction").innherHTML = player.fullClip
+        document.getElementById("liveAction").innerHTML = player.fullClip;
     } else {
     player.holster -= 1;
     player.loaded += 1;
-    playerLoaded = document.getElementById("playerLoaded").innerHTML = player.loaded
+        document.getElementById("playerLoaded").innerHTML = player.loaded
     document.getElementById("playerHolster").innerHTML = player.holster;
+        enemyIsNotLoaded();
     }
-}
 
+}
 
 document.getElementById("reloadBtn").onclick = function () {
     // count -= 10;
